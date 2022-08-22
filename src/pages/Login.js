0a72894ce7d0b94869139187cb3920c38524c3a4
kruti-paneri93 <100898553+kruti-paneri93@users.noreply.github.com>
2022-08-22 12:11:56 +0530

    Update Login.js

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,50 +1,95 @@
-// import { Component } from 'react';
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-import React , {useState} from 'react'
-// import {useNavigate} from 'react-router-dom';
-function Login() {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    // const history = useNavigate();
-    
-    async function entry(){
-        console.log(email , password);
-        // let item = {email , password};
-        // let result = await fetch ("http://3.7.50.43:3000/API/login" , {
-        //     method : 'POST',
-        //     headers : {
-        //         "Content-Type" : "application/json",
-        //         "Content-Length" : "<calculated when request is sent>",
-        //         "Host" : "<calculated when request is sent>",
-        //         "User-Agent" : "PostmanRuntime/7.29.2",
-        //         "Accept" : "*/*",
-        //         "Accept-Encoding" : "gzip, deflate, br",
-        //         "Connection" : "keep-alive"
-        //     },
-        //     body : JSON.stringify(item)
-        // });
-        // result = await result.json();
-        // localStorage.setItem(JSON.stringify(result))
-        // history.pushState("/home")
-    }
-  return (
-    <div className='container'>
-        <div className='d-flex align-items-center justify-content-center'>
-            <Form className='col-6 form-height form-position'>
-                <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <Form.Control type="email" placeholder="Enter email" onChange={(e) => setEmail(e.target.value)} />
-                </Form.Group>
+import React from 'react'
+import { useState } from "react";
 
-                <Form.Group className="mb-3" controlId="formBasicPassword">
-                    <Form.Control type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
-                </Form.Group>
-                <Button variant="primary" type="submit" onClick={entry} >
-                    Submit
-                </Button>
-            </Form>
+
+function Login() {
+    const initialValues = { email: "", password: "" };
+    const [formValues, setFormValues] = useState(initialValues);
+    const [formErrors, setFormErrors] = useState({});
+    const [isSubmit, setIsSubmit] = useState(false);
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormValues({ ...formValues, [name]: value });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setFormErrors((formValues));
+        setIsSubmit(true);
+        fetch ("http://3.7.50.43:3000/API/login" , {
+            method: 'POST',
+            headers: {
+                'Content-Type' : 'application/json'
+            },
+            body: JSON.stringify(formValues)
+        })
+            .then((res) => {
+                return res.json()
+            })
+            .then((formValues) => console.log(formValues))
+            .catch(error => console.log('ERROR'))
+    }
+
+    // useEffect(() => {
+    //     console.log(formErrors);
+    //     if (Object.keys(formErrors).length === 0 && isSubmit) {
+    //         console.log(formValues);
+    //     }
+    // }, [formErrors]);
+    
+    return (
+        <>
+        <div className="App-container">
+            <div className="card">
+                {Object.keys(formErrors).length === 0 && isSubmit ? (
+                    <div className="ui message success">
+                        Signed in successfully!!
+                    </div>
+                ) : (
+                    <noscript>{JSON.stringify(formValues, undefined, 2)}</noscript>
+                )}
+                <div className="card-header">
+                    <h4 className="title">Login Form</h4>
+                </div>
+                <div className="card-body">
+                    <form className="form-style">
+
+                        <div className="ui divider"></div>
+                        <div className="ui form">
+
+
+                            <div className="field">
+                                <label>Email</label>
+                                <input
+                                    type="text"
+                                    name="email"
+                                    placeholder="Email"
+                                    value={formValues.email}
+                                    onChange={handleChange}
+                                />
+                            </div>
+                            <p className="error-msg">{formErrors.email}</p>
+                            <div className="field">
+                                <label>Password</label>
+                                <input
+                                    type="password"
+                                    name="password"
+                                    placeholder="Password"
+                                    value={formValues.password}
+                                    onChange={handleChange}
+                                />
+                            </div>
+                            <p className="error-msg">{formErrors.password}</p>
+                            <button className="btn" onClick={handleSubmit}>Submit</button>
+                        </div>
+                    </form>
+                    
+                </div>
+            </div>
         </div>
-    </div>
-  )
+        </>
+    )
 }
+
 export default Login
